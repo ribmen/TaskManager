6d88206fe78ac922af0f595fb4572182b518a08b
ribmen <v.g.menezes100@gmail.com>
2025-02-14 09:45:23 -0300

    feat: adding input fields

--- src/main/java/com/esig/taskmanager/controller/TaskController.java
+++ src/main/java/com/esig/taskmanager/controller/TaskController.java
@@ -1,16 +1,42 @@
 package com.esig.taskmanager.controller;
 
+import com.esig.taskmanager.model.entity.Task;
 import com.esig.taskmanager.service.ITaskService;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.List;
 
+@Named
+@ViewScoped
 public class TaskController implements Serializable {
 
+    @Inject
     private ITaskService taskService;
 
-    public void save() {
+    @Getter
+    @Setter
+    private Task task = new Task(); // Objeto para cadastro
 
+    @Getter
+    private List<Task> taskList; // Lista de tarefas
+
+    public void save() {
+        taskService.saveTask(task);
+        task = new Task(); // Reseta o formulário
+        loadTasks(); // Atualiza a lista na view
     }
 
+    public void loadTasks() {
+        taskList = taskService.listTasks();
+    }
 
+    public void delete(Task task) {
+        taskService.deleteTask(task);
+        loadTasks();
+    }
 }
--- src/main/java/com/esig/taskmanager/service/impl/TaskServiceImpl.java
+++ src/main/java/com/esig/taskmanager/service/impl/TaskServiceImpl.java
@@ -4,9 +4,11 @@ import com.esig.taskmanager.model.entity.Task;
 import com.esig.taskmanager.repository.ITaskRepository;
 import com.esig.taskmanager.service.ITaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class TaskServiceImpl implements ITaskService {
 
     @Autowired
